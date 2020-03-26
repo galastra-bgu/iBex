0650bf3bfa6e49d66ae70105ea67707142221e90
galastra <galastra@post.bgu.ac.il>
2020-03-26 16:05:43 +0200

    made train_classifier more generic

--- main_program/classification/train_classifier.py
+++ main_program/classification/train_classifier.py
@@ -20,60 +20,38 @@ class OverSamplingCallback(LearnerCallback):
         sampler = WeightedRandomSampler(self.weights, self.total_len_oversample)
         self.data.train_dl = dl.new(shuffle=False, sampler=sampler)
 
-bs = 24 
+#init learn
+def trainfor(num_epochs,size):
+    np.random.seed(33)
+    torch.random.manual_seed(33)
+    data : ImageDataBunch= ImageDataBunch.from_folder("./image_data",train='.',valid_pct=0.2, ds_tfms=get_transforms(flip_vert=False), size=size, bs=bs,num_workers=4).normalize(imagenet_stats)
+    #beware of num_workers, many errors are because of it
+    learn.data = data
+    learn.lr_find()
+    learn.recorder.plot(suggestion=True)
+    try:
+        min_grad_lr = learn.recorder.min_grad_lr
+    except:
+        min_grad_lr = 3e-5
+    learn.fit_one_cycle(num_epochs, min_grad_lr,callbacks=[SaveModelCallback(learn, every='epoch', monitor='error_rate')])
 
-size = 100
-np.random.seed(33)
-torch.random.manual_seed(33)
-data : ImageDataBunch= ImageDataBunch.from_folder("./image_data",train='.',valid_pct=0.2, ds_tfms=get_transforms(flip_vert=False), size=size, bs=bs,num_workers=4).normalize(imagenet_stats)
-#beware of num_workers, many errors are because of it
+if __name__ == "__main__":
+    bs = 24 
 
-## Training: resnet50
+    data : ImageDataBunch= ImageDataBunch.from_folder("./image_data",train='.',valid_pct=0.2, ds_tfms=get_transforms(flip_vert=False), size=50, bs=bs,num_workers=4).normalize(imagenet_stats)
+    learn = cnn_learner(data, models.resnet50, metrics=[accuracy,Precision(),Recall(),FBeta()] , callback_fns=[OverSamplingCallback])
 
-learn = cnn_learner(data, models.resnet50, metrics=[accuracy,Precision(),Recall(),FBeta()] , callback_fns=[OverSamplingCallback])
-learn.path = Path("./learners/endgame/frozen")
-
-learn.lr_find()
-learn.recorder.plot(suggestion=True)
-min_grad_lr = learn.recorder.min_grad_lr
-
-print('*** started training frozen... ***')
-learn.fit_one_cycle(12, min_grad_lr,callbacks=[SaveModelCallback(learn, every='epoch', monitor='error_rate')])
-
-size = 256
-np.random.seed(33)
-torch.random.manual_seed(33)
-data : ImageDataBunch= ImageDataBunch.from_folder("./image_data",train='.',valid_pct=0.2, ds_tfms=get_transforms(flip_vert=False), size=size, bs=bs,num_workers=4).normalize(imagenet_stats)
-learn.path = Path("./learners/endgame/unfrozen")
-learn.unfreeze()
-learn.lr_find()
-learn.recorder.plot(suggestion=True)
-try:
-    min_grad_lr = learn.recorder.min_grad_lr
-except:
-    min_grad_lr = 3e-5
-print('*** started unfrozen... ***')
-
-learn.fit_one_cycle(4, min_grad_lr,callbacks=[SaveModelCallback(learn, every='epoch', monitor='error_rate')])
-
-#Now we make the size larger
-size = 512
-np.random.seed(33)
-torch.random.manual_seed(33)
-data : ImageDataBunch= ImageDataBunch.from_folder("./image_data",train='.',valid_pct=0.2, ds_tfms=get_transforms(flip_vert=False), size=size, bs=bs,num_workers=4).normalize(imagenet_stats)
-learn.data = data
-
-learn.path = Path('./learners/endgame/bigger')
-learn.lr_find()
-learn.recorder.plot(suggestion=True)
-try:
-    min_grad_lr = learn.recorder.min_grad_lr
-except:
-    min_grad_lr = 1e-6
-print('*** started unfrozen-bigger.... ***')
-learn.fit_one_cycle(6, min_grad_lr,callbacks=[SaveModelCallback(learn, every='epoch', monitor='error_rate')])
-
-
-
-
-learn.export()
+    print('******************FROZEN*********************')
+    learn.path=Path("./learners/endgame/frozen")
+    trainfor(8,128)
+    print('******************FROZEN BIGGER *********************')
+    learn.path=Path("./learners/endgame/frozen-bigger")
+    trainfor(6,256)
+    learn.unfreeze()
+    print('******************UNFROZEN *********************')
+    learn.path=Path("./learners/endgame/unfrozen")
+    trainfor(6,256)
+    print('******************UNFROZEN BIGGER****
+    learn.path=Path("./learners/endgame/unfrozen-bigger")
+    trainfor(12,512)
+    learn.export()
