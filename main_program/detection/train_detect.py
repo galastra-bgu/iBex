89b892bfae9384a42e7690d2c4b8e71600f0a608
galastra <galastra@post.bgu.ac.il>
2020-04-01 00:56:49 +0300

    fixed import

--- main_program/detection/train_detect.py
+++ main_program/detection/train_detect.py
@@ -169,7 +169,7 @@ def train(num_epoch,freeze,weights=None):
 
     data_loader_test = torch.utils.data.DataLoader(
         dataset_test, batch_size=1, shuffle=False, num_workers=4,
-        collate_fn=collate_fn)
+        collate_fn=references.utils.collate_fn)
 
     # get the model using our helper function
     model = get_model(num_classes)
